c0a368cf1188b0883ca3f3c474f7a334c672d705
cesar daniel <cdanielmg200@gmail.com>
2019-05-16 14:06:19 -0500

    Se agregan las clases base de la automatización, aun sin implementar

---
+++ ejercicio3/src/main/java/com/cedaniel200/screenplay/questions/FlightConfirmation.java
@@ -0,0 +1,12 @@
+package com.cedaniel200.screenplay.questions;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+public class FlightConfirmation implements Question<Boolean> {
+
+    @Override
+    public Boolean answeredBy(Actor actor) {
+        return null;
+    }
+}
